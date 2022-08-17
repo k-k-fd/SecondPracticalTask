e4c576c89bed97d477d119c9e0bf718d501e4449
Khancharyan, Karen <karen.khancharyan@scotiabank.com>
2022-08-17 08:40:30 -0400

    replaced server.py. added client_server dir. added client_server to .gitignore

--- v1/server.py
+++ v1/server.py
@@ -1,44 +1,24 @@
 import socket
-import threading
+import time
+from datetime import datetime
 
-HEADER = 64
-PORT = 5050
-SERVER = socket.gethostbyname(socket.gethostname())
+
+# SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = '127.0.0.1'
+PORT = 3000
 ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                connected = False
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-
-    conn.close()
-
-
 def start():
     server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
+    print(f'Server has started on {SERVER}')
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        now = datetime.now()
+        now_str = now.strftime("%Y/%m/%d %H:%M:%S")
+        print(f'The server is on. IP:{SERVER} PORT:{PORT} DATE/TIME:{now_str} ...')
+        time.sleep(10)
 
 
-print("[STARTING] server is starting...")
 start()
