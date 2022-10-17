fe4d6e25c12c3755269865c094c99643faaf88c2
Khancharyan, Karen <karen.khancharyan.fd@gmail.com>
2022-10-17 16:06:21 -0400

    Modify: main.py by removing msg_create() not to return srting as per the request from reviewer

--- v2/main.py
+++ v2/main.py
@@ -15,18 +15,15 @@ def calc_radecdist(plnt_dst, plnt_src):
     moon, earth = load.planets[plnt_dst], load.planets[plnt_src]
     obsrvr = earth.at(t).observe(moon)
     ra, dec, dist = obsrvr.radec()
-    return ra, dec, dist
-
-
-def msg_create(planet_dst, planet_src):
-    ra, dec, dist = calc_radecdist(planet_dst, planet_src)
-    return f'RA: {ra}; DEC: {dec}'
+    return ra, dec
 
 
 async def echo(websocket):
     while True:
-        await websocket.send(msg_create(PLANET_DESTINATION, PLANET_SOURCE))
-        print(msg_create(PLANET_DESTINATION, PLANET_SOURCE))
+        ra, dec = calc_radecdist(PLANET_DESTINATION, PLANET_SOURCE)
+        msg = f'RA: {ra}; DEC: {dec}'
+        await websocket.send(msg)
+        print(msg)
         await asyncio.sleep(SLEEP_TIME)
 
 
@@ -34,7 +31,9 @@ def main():
     load.planets = load('de421.bsp')
     ngrok_conn = ngrok.connect(PORT, bind_tls=True)
     ngrok_url = ngrok_conn.public_url
-    print(msg_create(PLANET_DESTINATION, PLANET_SOURCE))
+    ra, dec = calc_radecdist(PLANET_DESTINATION, PLANET_SOURCE)
+    msg = f'RA: {ra}; DEC: {dec}'
+    print(msg)
     print(ngrok_url)
     websockets_serve = websockets.serve(echo, SERVER, PORT)
     asyncio.get_event_loop().run_until_complete(websockets_serve)
